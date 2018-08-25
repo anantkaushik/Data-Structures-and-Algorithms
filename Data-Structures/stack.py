e7a5129ac5e754664b72c264c642220b6535072a
Anant Kaushik <anant.kaushik2@gmail.com>
2018-08-26 00:16:11 +0530

    Stack

---
+++ Data-Structures/stack.py
@@ -0,0 +1,22 @@
+class Stack:
+    arr = []
+    def push(self,item):
+        self.arr.append(item)
+    
+    def spop(self):
+        if len(self.arr) > 0:
+            return self.arr.pop()
+        else:
+            return #stack is empty
+    
+    def dispaly(self):
+        print(self.arr)
+
+x = Stack() # Creating object of Stack class
+x.push(1) # Pushing 1 to stack
+x.push(2)  # Pushing 2 to stack
+x.dispaly() # arr = [1,2]
+x.spop() # Deleting the last element from the stack.
+x.dispaly() # arr = [1]
+print(x.spop()) # 1
+print(x.spop()) # None(because stack is already empty)
