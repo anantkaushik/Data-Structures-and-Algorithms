36319de23e7571cddf62f8e80663d5b8b23656d6
Anant Kaushik <anant.kaushik2@gmail.com>
2018-09-29 10:56:50 +0530

    Binary Search Tree

---
+++ Data-Structures/Tree/Binary-Search-Tree.py
@@ -0,0 +1,33 @@
+"""
+------------------------ Binary Search Tree -------------------------
+"""
+class Node:
+	def __init__(self, val):
+		self.left = None
+		self.right = None
+		self.data = val
+
+class Binary_Tree:
+	def __init__(self):
+		self.root = None
+
+	def getRoot(self):
+		return self.root
+
+	def add(self, val):
+		if self.root == None:
+			self.root = Node(val)
+		else:
+			self._add(val, self.root)
+
+	def _add(self, val, node):
+		if val < node.data:
+			if node.left != None:
+				self._add(val, node.left)
+			else:
+				node.left = Node(val)
+		else:
+			if node.right != None:
+				self._add(val, node.right)
+			else:
+				node.right = Node(val)
