1560e91d4889cae3304b0be23beefcc4ae751e36
Anant Kaushik <anant.kaushik2@gmail.com>
2019-06-26 12:42:02 +0530

    Kadane's Algorithm

--- Algorithms/Kadane's-Algorithm.py
+++ Algorithms/Kadane's-Algorithm.py
@@ -1,7 +1,7 @@
 """
 Kadane’s algorithm is a Dynamic Programming approach to solve “the largest contiguous elements in an array” with runtime of O(n).
 """
-def maxSubArray(self, nums):
+def maxSubArray(nums):
     if len(nums) == 0:
         return 0
     Sum = nums[0]
